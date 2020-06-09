f299ac477b1825b1c5d0643b12a8b491c67faaeb
Artem <artemedmshot@gmail.com>
2020-06-09 19:28:35 +0300

    Last result

---
+++ ExchangeRates/Extension/UIColor+Extension.swift
@@ -0,0 +1,26 @@
+//
+//  UIColor+Extension.swift
+//  ExchangeRates
+//
+//  Created by admin on 6/8/20.
+//  Copyright © 2020 Artem Pozdnyakov. All rights reserved.
+//
+
+import UIKit
+
+
+extension UIColor {
+
+    static func turquoise() -> UIColor {
+        return #colorLiteral(red: 0, green: 0.82681638, blue: 0.7845831513, alpha: 1)
+    }
+    
+    static func idealGrey() -> UIColor {
+        return #colorLiteral(red: 0.3019607843, green: 0.3215686275, blue: 0.337254902, alpha: 1)
+    }
+    
+    
+    static func idealBlack() -> UIColor {
+        return #colorLiteral(red: 0.02745098039, green: 0.03137254902, blue: 0.0431372549, alpha: 1)
+    }
+}
---
+++ ExchangeRates/Model/CourseDataModel.swift
@@ -0,0 +1,32 @@
+//
+//  CourseDataModel.swift
+//  ExchangeRates
+//
+//  Created by admin on 6/4/20.
+//  Copyright © 2020 Artem Pozdnyakov. All rights reserved.
+//
+
+import Foundation
+
+struct CourseDataModel: Codable {
+    let date: String
+    let valute: [String: Valutes]
+
+    enum CodingKeys: String, CodingKey {
+        case date = "Date"
+        case valute = "Valute"
+    }
+}
+
+// MARK: - Valute
+struct Valutes: Codable {
+    let charCode: String
+    let valuettt: Double
+    let fullName: String
+
+    enum CodingKeys: String, CodingKey {
+        case charCode = "CharCode"
+        case valuettt = "Value"
+        case fullName = "Name"
+    }
+}
